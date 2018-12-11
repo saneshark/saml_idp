57877aaacaea0439e2d2c1c915edbde53eccab22
Kam Karshenas <kam@adhocteam.us>
2018-12-11 01:38:44 -0800

    fix for idp_controller spec

--- spec/acceptance/idp_controller_spec.rb
+++ spec/acceptance/idp_controller_spec.rb
@@ -2,15 +2,11 @@ require File.expand_path(File.dirname(__FILE__) + '/acceptance_helper')
 
 feature 'IdpController' do
   scenario 'Login via default signup page' do
-    Capybara.register_driver :chrome do |app|
-      Capybara::Selenium::Driver.new(app, :browser => :chrome)
-    end
-    Capybara.current_driver = :chrome
     saml_request = make_saml_request("http://foo.example.com/saml/consume")
     visit "/saml/auth?SAMLRequest=#{CGI.escape(saml_request)}"
-
-    fill_in 'Email', :with => "foo@example.com"
-    fill_in 'Password', :with => "okidoki"
+    expect(status_code).to eq(200)
+    fill_in 'email', :with => "foo@example.com"
+    fill_in 'password', :with => "okidoki"
     click_button 'Sign in'
     click_button 'Submit'   # simulating onload
     expect(current_url).to eq('http://foo.example.com/saml/consume')
